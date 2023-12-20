8d88cabdf71a3f9d89681ccc3b24387becc2cec5
taylor-ben <benjamin.taylor@mail.huji.ac.il>
2023-12-20 15:14:12 +0200

    eventReaction

--- index.ts
+++ index.ts
@@ -1,28 +1,34 @@
 import { useEffect, useState } from "react";
 import { BehaviorSubject, Subject } from "rxjs";
 
-export const useBehaviorSubject = <T>(
-  behaviorSubject: BehaviorSubject<T>
+export const useBehaviorSubject = <T,>(
+  behaviorSubject: BehaviorSubject<T>,
+  eventReaction?: (value: T) => void
 ): [T, (value: T) => void] => {
   const [currentState, setCurrentState] = useState(behaviorSubject.value);
   useEffect(() => {
-    const subscription = behaviorSubject.subscribe((value) =>
+    const subscription = behaviorSubject.subscribe((value) => {
       setCurrentState(value)
-    );
+      eventReaction?.(value)
+    });
     return () => subscription.unsubscribe();
   }, [behaviorSubject]);
   const setState = (value: T) => behaviorSubject.next(value);
   return [currentState, setState];
 };
 
-export const useSubject = <T>(
+export const useSubject = <T,>(
   subject: Subject<T>,
   eventReaction?: (value: T) => void
-) => {
+): [T | undefined, (value: T) => void] => {
+  const [lastState, setLastState] = useState<T>()
   useEffect(() => {
-    const subscription = subject.subscribe((value) => eventReaction?.(value));
+    const subscription = subject.subscribe((value) => {
+      setLastState(value)
+      eventReaction?.(value)
+    });
     return () => subscription.unsubscribe();
   }, [subject]);
-  const fireEvent = (value: T) => subject.next(value);
-  return fireEvent;
+  const setState = (value: T) => subject.next(value);
+  return [lastState, setState];
 };
